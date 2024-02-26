e9def8db52fee54c387255ea18c68d1d4a066bab
Juan <correo122@correo.com>
2024-02-26 16:14:29 -0500

    js taller

---
+++ js/contenido.js
@@ -0,0 +1,28 @@
+function nuevoParrafo() {
+    var contenido = document.getElementById('contenido');
+    var texto = document.getElementById("texto").value;
+    contenido.innerHTML = contenido.innerHTML + '<p>' + texto + '</p>';
+}
+
+function nuevoTitulo(){
+    var contenido = document.getElementById('contenido');
+    var texto = document.getElementById("texto").value;
+    contenido.innerHTML = contenido.innerHTML + '<h1>' + texto + '</h1>';
+}
+
+document.getElementById('imagen').onchange = function (evt) {
+    var entradaArchivo = evt.target,
+        archivos = entradaArchivo.files;
+    if (FileReader && archivos && archivos.length) {
+        var imagen = new FileReader();
+        imagen.onload = function () {
+            var etiquetaImg = document.createElement("img");
+            etiquetaImg.setAttribute('src', imagen.result);
+            etiquetaImg.setAttribute('width', '200px');
+            etiquetaImg.setAttribute('height', '180px');
+            var contenido = document.getElementById('contenido');
+            contenido.appendChild(etiquetaImg);
+        }
+        imagen.readAsDataURL(archivos[0]);
+    }
+}
